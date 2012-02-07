6c3019b374be164e7413e69f0603f71e250e0af3
mabarroso <mabarroso@mabarroso.com>
2012-02-07 17:17:13 +0100

    Add Quota Checker

--- lib/true-random.rb
+++ lib/true-random.rb
@@ -3,14 +3,35 @@ require 'net/http'
 module TrueRandom
   class Random
     def integer n=1, min=1, max=100, base = 10
-      response = request "integers/?num=#{n}&min=#{min}&max=#{max}&col=1&base=#{base}"
+      return integers "integers/?num=#{n}&min=#{min}&max=#{max}&col=1&base=#{base}"
+    end
+
+    def quota ip = false
+    	if ip
+    		return integers "quota/?"
+    	else
+    		return integers "quota/?ip=#{ip}"
+    	end
+
+  	end
+
+    private
+    def integers uri
+      response = request uri
 
       return false unless response
       return response[0].to_i if response.count == 1
       return response.collect {|x| x.to_i }
     end
 
-    private
+    def strings uri
+      response = request uri
+
+      return false unless response
+      return response[0] if response.count == 1
+      return response
+    end
+
     def request uri
       http_response = Net::HTTP.get_response(URI.parse('http://www.random.org/' + uri + '&format=plain&rnd=new'))
       return false unless http_response.code.to_i == 200
